8e428b5a5a4a729514d67c66246d47bdfad9c338
MesuDevastator <3164432713@qq.com>
2025-07-21 21:19:07 +0800

    upload

--- js/Mousemove.js
+++ js/Mousemove.js
@@ -2,9 +2,20 @@ document.addEventListener('mousemove', BackGroundMove);
 function BackGroundMove(every) {
     document.querySelectorAll('.background').forEach((js1) => {
         const speed = parseFloat(js1.getAttribute('data-speed'));
-        const maxOffset = 50;
-        const x = Math.max(Math.min((window.innerWidth - every.pageX * speed) / 100, maxOffset), -maxOffset);
-        const y = Math.max(Math.min((window.innerHeight - every.pageY * speed) / 100, maxOffset), -maxOffset);
+        const bg_size = document.querySelector('.background');
+        const x_max = (bg_size.clientWidth);
+        const y_max = (bg_size.scrollHeight);
+        // const vertices = {
+        //     topLeft: { x: 0, y: 0 },
+        //     topright: { x: x_max, y: y_max },
+        //     bottomLeft: { x: 0, y: -y_max },
+        //     bottomRight: { x: x_max, y: -y_max }
+        // }
+        // const x = Math.max(Math.min(window.innerWidth - every.pageX * speed) / 100);
+        // const y = Math.max(Math.min(window.innerHeight - every.pageY * speed) / 100);
+        const x = Math.max(Math.min(x_max - every.pageX * speed) / 100);
+        const y = Math.max(Math.min(y_max - every.pageY * speed) / 100);
         js1.style.transform = `translateX(${x}px) translateY(${y}px)`;
+        js1.style.width = `width(${x_max}) height(${y_max})`;
     })
 }
