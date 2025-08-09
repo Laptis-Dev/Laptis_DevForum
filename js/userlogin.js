07942a26d17013c63ed5dd9ec9e6f7b897b47fc0
MitterGYanik <ymx09025@gmail.com>
2025-08-10 05:06:29 +0800

    The last major update in August (regarding the login and registration interface)

--- js/userlogin.js
+++ js/userlogin.js
@@ -1,5 +1,13 @@
-class name {
-    constructor(parameters) {
+var signUpButton = document.getElementById('register');
+var signinButton = document.getElementById('login');
+var container = document.getElementById('container');
 
-    }
-}
+signinButton.addEventListener('click', function () {
+    container.classList.remove(`left-panel-active`);
+    container.classList.add('right-panel-active');
+});
+
+signUpButton.addEventListener('click', function () {
+    container.classList.remove(`right-panel-active`);
+    container.classList.add(`left-panel-active`);
+});
